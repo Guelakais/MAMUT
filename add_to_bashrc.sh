41fdd9561a13f77c31285f4183b3b279b00d5fec
Guelakais <koroyeldiores@gmail.com>
2023-03-17 12:28:31 +0100

    Add files via upload

---
+++ add_to_bashrc.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+BASHRC_FILE="$HOME/.bashrc"
+
+# Define an array of lines to add to the .bashrc file
+declare -a lines=(
+    "source /opt/ros/humble/setup.bash"
+    "export ROS_DOMAIN_ID=30"
+    ". \"$HOME/.cargo/env\""
+    "source /usr/share/colcon_cd/function/colcon_cd.sh"
+    "export _colcon_cd_root=/opt/ros/humble/"
+    "source /usr/share/colcon_argcomplete/hook/colcon-argcomplete.bash"
+    "export LDS_MODEL=LDS-01"
+    "export OPENCR_PORT=/dev/ttyACM0"
+    "export OPENCR_MODEL=burger"
+    "export MAKEFLAGS=\"-j1\""
+)
+
+# Append lines to the end of the .bashrc file using a for loop
+for line in "${lines[@]}"
+do
+    echo "$line" >> "$BASHRC_FILE"
+done
---
+++ basicInstall.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+rosDistro="humble"
+sudo apt update
+sudo apt upgrade
+sudo apt full-upgrade
+sudo apt install locales
+sudo locale-gen en_US en_US.UTF-8
+sudo update-locale LC_ALL=en_US.UTF-8 LANG=en_US.UTF-8
+sudo apt install software-properties-common
+sudo add-apt-repository universe
+sudo apt install curl
+sudo curl -sSL https://raw.githubusercontent.com/ros/rosdistro/master/ros.key -o /usr/share/keyrings/ros-archive-keyring.gpg
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/ros-archive-keyring.gpg] http://packages.ros.org/ros2/ubuntu $(. /etc/os-release && echo $UBUNTU_CODENAME) main" | sudo tee /etc/apt/sources.list.d/ros2.list > /dev/null
+sudo apt install ros-humble-ros-base
+sudo apt install ros-dev-tools
+sudo apt install libudev-dev
+sudo apt install libc6:armhf
+sudo apt install ros-${rosDistro}-cartographer
+sudo apt install ros-${rosDistro}-cartographer-ros
+sudo apt install ros-${rosDistro}-navigation2
+sudo apt install ros-${rosDistro}-nav2-bringup
+sudo apt install ros-${rosDistro}-dynamixel-sdk
+sudo apt install ros-${rosDistro}-turtlebot3-msgs
+sudo apt install ros-${rosDistro}-turtlebot3
+sudo dpkg --add-architecture armhf
