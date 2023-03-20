a16f9b494c22977e29184a4f6cd12ea92a532b23
Guelakais <koroyeldiores@gmail.com>
2023-03-20 12:18:59 +0100

    Update basicInstall.sh

--- basicInstall.sh
+++ basicInstall.sh
@@ -11,7 +11,7 @@ sudo add-apt-repository universe
 sudo apt install curl
 sudo curl -sSL https://raw.githubusercontent.com/ros/rosdistro/master/ros.key -o /usr/share/keyrings/ros-archive-keyring.gpg
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/ros-archive-keyring.gpg] http://packages.ros.org/ros2/ubuntu $(. /etc/os-release && echo $UBUNTU_CODENAME) main" | sudo tee /etc/apt/sources.list.d/ros2.list > /dev/null
-sudo apt install ros-humble-ros-base
+sudo apt install ros-${rosDistro}-ros-base
 sudo apt install ros-dev-tools
 sudo apt install libudev-dev
 sudo apt install libc6:armhf
