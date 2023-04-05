4e4b43aa2278595629d76ddd49f3d25852dbcdb4
Guelakais <koroyeldiores@gmail.com>
2023-04-05 12:09:10 +0200

    updated removables

--- basicInstall.sh
+++ basicInstall.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
-sudo apt remove --auto-remove --purge libreoffice* thunderbird update-manager update-manager-core update-notifier* rhythmbox gamemode* aisleriot cups* gnome-mines gnome-calculator gnome-calendar brltty-x11 xbrlapi python3-brlapi
+sudo apt remove --auto-remove --purge libreoffice* thunderbird update-manager \
+update-manager-core update-notifier* rhythmbox gamemode* aisleriot cups* \
+gnome-mines gnome-calculator gnome-calendar gnome-todo gnome-keyring gnome-keyring-pkcs11 \
+gnome-bluetooth gnome-bluetooth-3-common gnome-bluetooth-common gnome-sudoku brltty-x11 \
+xbrlapi python3-brlapi
 rosDistro="humble"
 sudo apt update
 sudo apt upgrade
@@ -10,7 +14,8 @@ sudo update-locale LC_ALL=en_US.UTF-8 LANG=en_US.UTF-8
 sudo apt install software-properties-common
 sudo add-apt-repository universe
 sudo apt install curl
-sudo curl -sSL https://raw.githubusercontent.com/ros/rosdistro/master/ros.key -o /usr/share/keyrings/ros-archive-keyring.gpg
+sudo curl -sSL https://raw.githubusercontent.com/ros/rosdistro/master/ros.key -o \
+/usr/share/keyrings/ros-archive-keyring.gpg
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/ros-archive-keyring.gpg] http://packages.ros.org/ros2/ubuntu $(. /etc/os-release && echo $UBUNTU_CODENAME) main" | sudo tee /etc/apt/sources.list.d/ros2.list > /dev/null
 sudo apt update
 sudo apt upgrade
